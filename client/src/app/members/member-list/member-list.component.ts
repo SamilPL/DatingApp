12a7eb31567e9b858f4184b58a3ef93b62e76890
Sandra Palacio <sandra_palacio@dialog.com.au>
2020-10-27 08:47:05 +1000

    En of Section 3 - Pagination

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Member } from 'src/app/_models/member';
+import { Pagination } from 'src/app/_models/Pagination';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -9,13 +11,30 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]>;
+  members: Member[];
+  pagination: Pagination;
+  pageNumber = 1;
+  pageSize = 5;
 
   constructor(
     private memberService: MembersService
   ) { }
 
   ngOnInit(): void {
-    this.members$ = this.memberService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers(this.pageNumber, this.pageSize).pipe(
+      map(response => {
+        this.members = response.result;
+        this.pagination = response.pagination;
+      })
+    ).subscribe();
+  }
+
+  pageChanged(event: any) {
+    this.pageNumber = event.page;
+    this.loadMembers();
   }
 }
