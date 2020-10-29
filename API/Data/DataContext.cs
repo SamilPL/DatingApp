41e0b11c104fbc0b5a43eeff81dcdd90806fb777
Sandra Palacio <sandra_palacio@dialog.com.au>
2020-10-29 11:18:42 +1000

    Section 14 - Added UserLike entity

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,36 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        /* We need to override this method which is inside DbContext to configure
+            our UserLike table
+        */
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserLike>()
+                //We have to specify which is going to be the PK since we did not do it 
+                // on the entity and in this case it is going to be a combination.
+                .HasKey(k => new {k.SourceUserId, k.LikedUserId});
+
+            // Configurate the relationship
+            // 1. A source user can like many other users
+             builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade); // if we delete an User we delete the related entities
+                // If using SQL Server,  the DeleteBehavior has to be set to DeleteBehavior.NoAction 
+            
+            // 2. A liked user can have many liked by users
+             builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade); // if we delete an User we delete the related entities
+        }
+    
     }
 }
